54d46a9031a1bcdf4513e16e9900d4931c4c36a9
tmtri3102 <tmtri310251@gmail.com>
2024-10-21 16:37:00 +0700

    Test 2

--- test/TriangleClassifierTest.java
+++ test/TriangleClassifierTest.java
@@ -16,7 +16,16 @@ class TriangleClassifierTest {
         assertEquals(expected, result);
     }
 
-
+    @Test
+    @DisplayName("Test tam giac can")
+    void testTamGiacCan() {
+        int a = 2;
+        int b = 2;
+        int c = 3;
+        String expected = "Tam giac can";
+        String result = classifier.classifyTriangle(a, b, c);
+        assertEquals(expected, result);
+    }
 
 
 }
