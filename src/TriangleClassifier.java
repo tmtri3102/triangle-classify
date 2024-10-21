a13d0e6e63f20966da6a92b99f2e2b7f3bd1fcfd
tmtri3102 <tmtri310251@gmail.com>
2024-10-21 16:34:50 +0700

    Test 1

---
+++ src/TriangleClassifier.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TriangleClassifier {
+    public static String classifyTriangle(int a, int b, int c) {
+        if (a + b > c || a + c > b || b + c > a) {
+            if (a == b && b == c) {
+                return "Tam giac deu";
+            } else if ((a == b) || (b == c) || (c == a)) {
+                return "Tam giac can";
+            } else {
+                return "Tam giac thuong";
+            }
+        } else {
+            return "Khong phai tam giac";
+        }
+    }
+}
---
+++ test/TriangleClassifierTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleClassifierTest {
+    TriangleClassifier classifier = new TriangleClassifier();
+    @Test
+    @DisplayName("Test tam giac deu")
+    void testTamGiacDeu() {
+        int a = 2;
+        int b = 2;
+        int c = 2;
+        String expected = "Tam giac deu";
+        String result = classifier.classifyTriangle(a, b, c);
+        assertEquals(expected, result);
+    }
+
+
+
+
+}
